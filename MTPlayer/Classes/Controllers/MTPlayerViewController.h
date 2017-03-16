5cc590d47bdcb5507e5d07c7ab2e49ca2280b82c
肖伟 <1097526615@qq.com>
2017-03-16 13:58:26 +0800

    建好项目结构

---
+++ MTPlayer/Classes/Controllers/MTPlayerViewController.h
@@ -0,0 +1,13 @@
+//
+//  MTPlayerViewController.h
+//  MTPlayer
+//
+//  Created by Metallic  on 17/3/16.
+//  Copyright © 2017年 xiaowei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MTPlayerViewController : UIViewController
+
+@end
